74b7cbb52a4c450c6c43fc2226c6bdc3abfb73e7
minakshiSalave2388 <minakshi.salave@gmail.com>
2024-04-11 22:15:45 +1000

    added files

---
+++ src/test/java/utils/TestBase.java
@@ -0,0 +1,46 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.GeckoDriverInfo;
+
+public class TestBase {
+	public WebDriver driver;
+	
+	public WebDriver WebDriverManager() throws IOException
+	{
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"//src//test//resources//global.properties");
+		Properties prop = new Properties();
+		prop.load(fis);
+		String url = prop.getProperty("QAUrl");
+		String browser_properties = prop.getProperty("browser");
+		String browser_maven = System.getProperty("browser");
+		String browser= browser_maven!=null? browser_maven:browser_properties;
+		if(driver== null)
+		{
+			if(browser.equalsIgnoreCase("chrome"))
+			{
+				driver =new ChromeDriver();
+			}
+			else if(browser.equalsIgnoreCase("edge"))
+			{
+				driver =(WebDriver) new GeckoDriverInfo();
+			}
+			else if(browser.equalsIgnoreCase("firefox"))
+			{
+				driver =new FirefoxDriver();
+			}
+		}
+		driver.get(url);
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.manage().window().maximize();
+		return driver;
+	}
+
+}
---
+++ target/test-classes/features/searchProduct.feature
@@ -0,0 +1,14 @@
+Feature: Search product and place the order with that product
+
+@OffersPage
+Scenario Outline: Search experience for product both in Home page and Offers page
+
+Given User is on GreenCart Landing page
+When user searched with shortname <vegName> and extracted actual name of product
+Then user searched for <vegName> in offers page
+And product name matches with the landing page and offer page
+
+Examples:
+|vegName|
+|Tom|
+
